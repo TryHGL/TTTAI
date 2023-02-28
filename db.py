b2554f590e646039efcf7038a5f8e55d64666252
Nichita Cusnir <nikitacusnir06@gmail.com>
2023-02-28 17:23:03 +0100

    player turns logic + gui

--- db.py
+++ db.py
@@ -1,15 +1,23 @@
+ICON = "ttticon.jpg"
 # display
 WIDTH = 600
 HEIGHT = 600
 
-BG_COLOR = (20, 200, 140)
-
-ICON = "ttticon.jpg"
-
 ROWS = 3
 COLS = 3
 
-SQUARE_SIZE = WIDTH//COLS
-# lines
-LINE_COLOR = (15, 150, 105)
+SQSIZE = WIDTH//COLS
+
 LINE_WIDTH = 10
+FIG_WIDTH = 20
+
+RADIUS = SQSIZE // 2.5
+
+OFFSET = SQSIZE//5
+
+
+# colors
+BG_COLOR = (20, 200, 140)
+LINE_COLOR = (15, 150, 105)
+CIRCLE_COLOR = (220, 220, 220)
+CROSS_COLOR = (66, 66, 66)
--- main.py
+++ main.py
@@ -19,28 +19,50 @@ class Board():
     def mask_sqr(self, row, col, player):
         self.squares[row][col] = player
 
+    def sq_empty(self, row, col):
+        return self.squares[row][col] == 0
+
 
 class Game:
-    def __init__(self, board) -> None:
-        self.board = board()
+    def __init__(self) -> None:
+        self.board = Board()
         self.draw_lines()
-        self.player = 1
+        self.player = 1  # 1 => circle; 2 => cross
 
-    def draw_lines(self):
+    def draw_lines(self) -> None:
         pygame.draw.line(screenttt, LINE_COLOR,
-                         (SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), LINE_WIDTH)
+                         (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
         pygame.draw.line(screenttt, LINE_COLOR,
-                         (2*SQUARE_SIZE, 0), (2*SQUARE_SIZE, HEIGHT), LINE_WIDTH)
+                         (2*SQSIZE, 0), (2*SQSIZE, HEIGHT), LINE_WIDTH)
 
         pygame.draw.line(screenttt, LINE_COLOR,
-                         (0, SQUARE_SIZE), (WIDTH, SQUARE_SIZE), LINE_WIDTH)
+                         (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screenttt, LINE_COLOR,
-                         (0, 2*SQUARE_SIZE), (WIDTH, 2*SQUARE_SIZE), LINE_WIDTH)
+                         (0, 2*SQSIZE), (WIDTH, 2*SQSIZE), LINE_WIDTH)
+
+    def draw_figure(self, row, col):
+        if self.player == 1:
+            # circle
+            center = (col * SQSIZE + SQSIZE // 2,
+                      row * SQSIZE + SQSIZE // 2)
+            pygame.draw.circle(screenttt, CIRCLE_COLOR, center,
+                               RADIUS, FIG_WIDTH)
+        if self.player == 2:
+            lup = (col * SQSIZE + OFFSET, row * SQSIZE + OFFSET)
+            ldn = (col * SQSIZE + OFFSET, row * SQSIZE + 4*OFFSET)
+            rup = (col * SQSIZE + 4*OFFSET, row * SQSIZE + OFFSET)
+            rdn = (col * SQSIZE + 4*OFFSET, row * SQSIZE + 4*OFFSET)
+            pygame.draw.line(screenttt, CROSS_COLOR, lup, rdn, FIG_WIDTH)
+            pygame.draw.line(screenttt, CROSS_COLOR, ldn, rup, FIG_WIDTH)
+
+    def next_turn(self):
+        self.player = self.player % 2 + 1
 
 
 def main():
 
     game = Game()
+    board = game.board
 
     while True:
 
@@ -50,12 +72,14 @@ def main():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = event.pos
-                row = pos[1] // SQUARE_SIZE
-                col = pos[0] // SQUARE_SIZE
-                if game.player == 1:
-                    game.player = 2
-                else:
-                    game.player == 1
+                row = pos[1] // SQSIZE
+                col = pos[0] // SQSIZE
+
+                if board.sq_empty(row, col):
+                    board.squares[row][col] = game.player
+                    game.draw_figure(row, col)
+                    game.next_turn()
+
         pygame.display.update()
 
 
